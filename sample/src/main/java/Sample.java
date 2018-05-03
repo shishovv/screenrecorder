310b71d7a4991206e227cc9db8124801cc14a5c7
Vladislav Shishov <vshov@hotmail.com>
2018-05-03 15:39:12 +0300

    update sample

--- sample/src/main/java/Sample.java
+++ sample/src/main/java/Sample.java
@@ -8,7 +8,7 @@ public class Sample {
     public static void main(String[] args) throws Exception {
         final Recorder recorder = ScreenRecorder.newRecorder(10, TimeUnit.SECONDS);
         recorder.startRecording();
-        Thread.sleep(30_000);
+        Thread.sleep(10_000);
         recorder.stopRecordingAndSave("sample.mov");
     }
 }
