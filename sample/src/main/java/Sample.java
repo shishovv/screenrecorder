6326e788cd77fcb053c3088c944c4bc97dbe4f93
Vladislav Shishov <vshov@hotmail.com>
2018-05-08 00:16:41 +0300

    update sample

--- sample/src/main/java/Sample.java
+++ sample/src/main/java/Sample.java
@@ -1,12 +1,16 @@
 import screenrecorder.Recorder;
 import screenrecorder.video.ScreenRecorder;
+import screenrecorder.video.VideoParams;
 
+import java.awt.*;
 import java.util.concurrent.TimeUnit;
 
 public class Sample {
 
     public static void main(String[] args) throws Exception {
-        final Recorder recorder = ScreenRecorder.newRecorder(10, TimeUnit.SECONDS);
+        final Dimension dimension = Toolkit.getDefaultToolkit().getScreenSize();
+        final Recorder recorder =
+                ScreenRecorder.newRecorder(VideoParams.newParams(dimension.width, dimension.height, 8), 10, TimeUnit.SECONDS);
         recorder.startRecording();
         Thread.sleep(10_000);
         recorder.stopRecordingAndSave("sample.mov");
